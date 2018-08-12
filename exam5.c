3a128ddb4091e22c4912f2539c27d104c5e838f4
myatsuphyo <myatsuphyo9@gmail.com>
2018-08-12 20:14:49 +0630

    Add files via upload

---
+++ exam5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void showBitStrings(int n) {
+/* Implement the body of this function.
+* Moreover, this function should call a recursive helper
+* function that solves the problem.
+*/
+}
+int main() {
+int n;
+scanf ("%d", &n); /* note: it is guaranteed that 0<n<=10 */
+showBitStrings(n);
+return 0;
+}
